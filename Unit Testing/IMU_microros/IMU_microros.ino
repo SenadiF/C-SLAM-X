e56702a3c081c42ae01259024e735e300c6ee810
SenadiF <sdfernando.70@gmail.com>
2026-07-23 22:49:36 +0530

    Added Encoder control

--- Unit Testing/IMU_microros/IMU_microros.ino
+++ Unit Testing/IMU_microros/IMU_microros.ino
@@ -5,8 +5,15 @@
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
+#include <robot_interfaces/msg/encoder.h>
 #include <sensor_msgs/msg/imu.h>
+
 #include <rosidl_runtime_c/string_functions.h>
+volatile long left_ticks = 0;
+volatile long right_ticks = 0;
+
+rcl_publisher_t encoder_publisher;
+robot_interfaces__msg__Encoder encoder_msg;
 
 
 // Constants for unit conversions
@@ -28,6 +35,20 @@ calData calib = {0};
 AccelData accelData;
 GyroData gyroData;
 
+//Encoder 
+#define LEFT_ENC_A 4 
+#define LEFT_ENC_B 13
+#define RIGHT_ENC_A 32 
+#define RIGHT_ENC_B 33
+
+//Motor driver 
+#define LEFT_MOTOR_IN1 25
+#define LEFT_MOTOR_IN2 26
+#define RIGHT_MOTOR_IN1 27
+#define RIGHT_MOTOR_IN2 14 
+
+
+
 // ROS objects
 rcl_allocator_t allocator;
 rclc_support_t support;
@@ -39,6 +60,43 @@ sensor_msgs__msg__Imu imu_msg;
 const uint32_t PUBLISH_PERIOD_MS = 10;
 unsigned long last_publish_time = 0;
 
+void IRAM_ATTR leftEncoderISR() {
+  bool a = digitalRead(LEFT_ENC_A);
+  bool b = digitalRead(LEFT_ENC_B);
+  left_ticks += (a == b) ? 1 : -1;
+}
+
+void IRAM_ATTR rightEncoderISR() {
+  bool a = digitalRead(RIGHT_ENC_A);
+  bool b = digitalRead(RIGHT_ENC_B);
+  right_ticks += (a == b) ? 1 : -1;
+}
+
+void setupEncoders() {
+  pinMode(LEFT_ENC_A, INPUT_PULLUP);   // GPIO4 supports internal pull-up
+  pinMode(LEFT_ENC_B, INPUT_PULLUP);   // GPIO13 supports internal pull-up
+  pinMode(RIGHT_ENC_A, INPUT_PULLUP);
+  pinMode(RIGHT_ENC_B, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(LEFT_ENC_A), leftEncoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_A), rightEncoderISR, CHANGE);
+  rclc_publisher_init_default(
+      &encoder_publisher,
+      &node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(robot_interfaces, msg, Encoder),
+      "encoder"
+  );
+}
+void setupMotors() {
+  pinMode(LEFT_MOTOR_IN1, OUTPUT);
+  pinMode(LEFT_MOTOR_IN2, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN1, OUTPUT);
+  pinMode(RIGHT_MOTOR_IN2, OUTPUT);
+  digitalWrite(LEFT_MOTOR_IN1, LOW);
+  digitalWrite(LEFT_MOTOR_IN2, LOW);
+  digitalWrite(RIGHT_MOTOR_IN1, LOW);
+  digitalWrite(RIGHT_MOTOR_IN2, LOW);
+}
 void setup()
 {
     Serial.begin(115200);
@@ -84,6 +142,12 @@ void setup()
     }
 
     Serial.println("BMI160 Initialized.");
+
+    setupEncoders();
+    Serial.println("Encoders Initialized");
+
+    setupMotors();
+    Serial.println("Motor pins Intilialized")
 }
 
 void loop()
@@ -135,7 +199,12 @@ void loop()
         imu_msg.linear_acceleration_covariance[4] = 0.04;
         imu_msg.linear_acceleration_covariance[8] = 0.04;
 
-        // Publish to your micro-ROS agent
+        // Publish 
         rcl_publish(&imu_publisher, &imu_msg, NULL);
+        
+       
+        encoder_msg.left_ticks = left_ticks;
+        encoder_msg.right_ticks = right_ticks;
+        rcl_publish(&encoder_publisher, &encoder_msg, NULL);
     }
 }

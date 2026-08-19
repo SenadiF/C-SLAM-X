e42bf86d509bb9a42901a2ddfc1bd7d0a373c86e
SenadiF <sdfernando.70@gmail.com>
2026-08-19 08:36:39 +0530

    Map merger  & multi robot slam updated .

--- robot_ws/src/my_navigation/launch/map_merge.launch.py
+++ robot_ws/src/my_navigation/launch/map_merge.launch.py
@@ -9,33 +9,35 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+
     ld = LaunchDescription()
+
     config = os.path.join(
-        get_package_share_directory("my_navigation"), "config", "map_merge_params.yaml"
+        get_package_share_directory("my_navigation"),
+        "config",
+        "map_merge_params.yaml"
     )
-    use_sim_time = LaunchConfiguration("use_sim_time")
+
     namespace = LaunchConfiguration("namespace")
     known_init_poses = LaunchConfiguration("known_init_poses")
 
-    declare_use_sim_time_argument = DeclareLaunchArgument(
-        "use_sim_time", default_value="true", description="Use simulation/Gazebo clock"
-    )
     declare_namespace_argument = DeclareLaunchArgument(
         "namespace",
         default_value="/",
-        description="Namespace for the explore node",
-    )
-    declare_known_init_poses_argument = DeclareLaunchArgument(
-        "known_init_poses",
-        default_value="True",
-        description="Known initial poses of the robots. If so don't forget to declare them in the params.yaml file",
+        description="Namespace for the map merge node",
     )
 
-    # Map fully qualified names to relative ones so the node's namespace can be prepended.
-    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
-    # https://github.com/ros/geometry2/issues/32
-    # https://github.com/ros/robot_state_publisher/pull/30
-    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+    declare_known_init_poses_argument = DeclareLaunchArgument(
+        "known_init_poses",
+        default_value="false",
+        description="Known initial poses of the robots",
+    )
+
+    
+    remappings = [
+        ("/tf", "tf"),
+        ("/tf_static", "tf_static")
+    ]
 
     node = Node(
         package="multirobot_map_merge",
@@ -44,14 +46,17 @@ def generate_launch_description():
         executable="map_merge",
         parameters=[
             config,
-            {"use_sim_time": use_sim_time},
-            {"known_init_poses": known_init_poses},
+            {
+                "use_sim_time": False,
+                "known_init_poses": known_init_poses,
+            }
         ],
         output="screen",
         remappings=remappings,
     )
-    ld.add_action(declare_use_sim_time_argument)
+
     ld.add_action(declare_known_init_poses_argument)
     ld.add_action(declare_namespace_argument)
     ld.add_action(node)
+
     return ld
--- robot_ws/src/robot_localization_config/launch/slam.launch.py
+++ robot_ws/src/robot_localization_config/launch/slam.launch.py
@@ -2,50 +2,45 @@ import os
 
 from launch import LaunchDescription
 from launch_ros.actions import Node, PushRosNamespace
-from launch.actions import DeclareLaunchArgument, GroupAction
-from launch.substitutions import LaunchConfiguration
+from launch.actions import GroupAction
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
 
-    namespace = LaunchConfiguration('namespace')
-
     pkg_share = get_package_share_directory(
         'robot_localization_config'
     )
 
-    slam_config = os.path.join(
+    slam_config_robot1 = os.path.join(
         pkg_share,
         'config',
-        'slam.yaml'
+        'slam1.yaml'
     )
-    
+
+    slam_config_robot2 = os.path.join(
+        pkg_share,
+        'config',
+        'slam2.yaml'
+    )
+
     return LaunchDescription([
+        #Robot 1 
 
-        DeclareLaunchArgument(
-            'namespace',
-            default_value='robot1',
-            description='Robot namespace'
-        ),
-
-        
         GroupAction(
             actions=[
-                PushRosNamespace(namespace), 
-                
+
+                PushRosNamespace('robot1'),
+
                 Node(
                     package='slam_toolbox',
                     executable='sync_slam_toolbox_node',
                     name='slam_toolbox',
                     output='screen',
-                   
+
                     parameters=[
-                        slam_config
-                    ],
-                     remappings=[
-        ('map', 'map')
-    ]
+                        slam_config_robot1
+                    ]
                 ),
 
                 Node(
@@ -53,7 +48,42 @@ def generate_launch_description():
                     executable='lifecycle_manager',
                     name='lifecycle_manager_slam',
                     output='screen',
-                   
+
+                    parameters=[
+                        {
+                            'autostart': True,
+                            'node_names': [
+                                'slam_toolbox'
+                            ]
+                        }
+                    ]
+                )
+            ]
+        ),
+        #Robot 2 
+
+        GroupAction(
+            actions=[
+
+                PushRosNamespace('robot2'),
+
+                Node(
+                    package='slam_toolbox',
+                    executable='sync_slam_toolbox_node',
+                    name='slam_toolbox',
+                    output='screen',
+
+                    parameters=[
+                        slam_config_robot2
+                    ]
+                ),
+
+                Node(
+                    package='nav2_lifecycle_manager',
+                    executable='lifecycle_manager',
+                    name='lifecycle_manager_slam',
+                    output='screen',
+
                     parameters=[
                         {
                             'autostart': True,
